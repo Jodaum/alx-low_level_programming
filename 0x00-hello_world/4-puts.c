ae324b4498b464fde6cab7a4658ff3865d71898c
Jodaum <jossyakakobb>
2022-12-08 16:19:12 -0800

    programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line using put 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilinqual puzzel");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <Stdio.h>
+/**
+ * Main :  prints the size of various types on the computer
+ *
+ *  Returm: 0
+ *  */
+int main(void)
+{
+		char a;
+		int b;
+		long int c;
+		long long int d;
+		float f;
+
+		printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+		printf("Size of an Int: %lu byte(s)\n", (unsigned long)sizeof(b));
+		printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+		printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+		return(0);
+}
