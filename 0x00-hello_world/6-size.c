3fae31d4f27273a7aa84c419c8c3998869f00c71
Jodaum <jossyakakobb>
2022-12-09 07:50:02 -0800

    y

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 /**
- * main: print using printf
- * name: code by josiah
- * Return: 0
- */
+ *  * main - write a C program that prints the size of various variables
+ *   * Return: 0
+ *   */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+	        printf("Size of a char: %lu byte(s)\n", sizeof(char));
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)\n",  (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n",  (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", +
-		(unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n",  (unsigned long)sizeof(f));
-return (0);
+		        printf("Size of an int: %lu byte(s)\n", sizeof(int));
+
+			        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+
+				        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+
+					        printf("Size of a float: %lu byte(s)\n", sizeof(float));
+						        return (0);
 }
-
