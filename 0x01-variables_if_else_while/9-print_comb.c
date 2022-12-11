0c6254b3b5ff4e1a6f8ec5a51a1f27892e30ab55
Jodaum <jossyakakobb>
2022-12-11 05:24:27 -0800

    y

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Returns: 0
-*/
+ * main - Entry point
+ *
+ * Return: 0
+ */
 
 int main(void)
-{
-int a;
+	{
+	int a;
 
-for (a = 0; a < 10; a++)
-{
-putchar(a + '0');
-if (a < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
-}
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a + '0');
+	if (a < 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+	}
