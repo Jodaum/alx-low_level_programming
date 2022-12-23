4dc9a03c6690dc57ed8c0a2b2e9eb5d3ff57c4dd
Jodaum <jossyakakobb>
2022-12-22 21:41:14 -0800

    y

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+/**
+* leet - Encode a string into 1337.
+* @str: The string to encode.
+* Return: A copy of the encoded string.
+*/
+char *leet(char *str)
+{
+int i = 0, j = 0;
+char tmp;
+int lc_letters[5] = {65, 69, 79, 84, 76};
+char corresps[5] = {'4', '3', '0', '7', '1'};
+
+do {
+tmp = str[i];
+for (j = 0; j < 5; j++)
+if ((tmp == lc_letters[j]) || (((int)tmp - 32) == lc_letters[j]))
+str[i] = corresps[j];
+i++;
+} while (tmp != '\0');
+return (str);
+}
