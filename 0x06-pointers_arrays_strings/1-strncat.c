e12b44acedf7082c703356f38c7495f1c8049c8e
Jodaum <jossyakakobb>
2022-12-23 19:52:13 -0800

    y

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,40 +1,30 @@
 #include "main.h"
-
 /**
- * _strlen - Returns the length of a string.
- * @s: The string whose the length should be returned.
- * Return: The length of s.
- */
-int _strlen(char *s)
-{
-int i = 0;
-char tmp;
-
-do {
-tmp = s[i];
-i++;
-} while (tmp != '\0');
-return (i - 1);
-}
-
-/**
- * _strncat - Concatenates two strings.
- * @dest: The first string and the destination.
- * @src: The second string.
- * @n: The number of byte to copy from src.
- * Return: dest + src[0 to n] in dest.
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, l = _strlen(dest), m = _strlen(src);
+	int i;
+	int j;
 
-if (n > m)
-n = m;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-do {
-dest[l + i] = src[i];
-i++;
-} while (i < n);
-return (dest);
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
-
