2ffb128a14f33195301e1d419b3b9bfa5ba2abe3
Jodaum <jossyakakobb>
2023-01-03 23:48:44 -0800

    y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,43 +1,16 @@
-#include "main.h"
 /**
- * _pow_recursion - power
- * @x:int
- * @y:int
- * Return:int
+ * _pow_recursion - Computes x raised to power of y.
+ * @x: The mantisse.
+ * @y: The exposant.
+ * Return: x ^ y.
  */
-
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	else if (y != 0)
-		return (x * _pow_recursion(x, y - 1));
-
-	else
-	{
+	else if (y == 0)
 		return (1);
-	}
-#include "main.h"
-/**
- * _pow_recursion - power
- * @x:int
- * @y:int
- * Return:int
- */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y != 0)
-		return (x * _pow_recursion(x, y - 1));
-
-	else
-	{
-		return (1);
-	}
-}}
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
